1b3ee39fb66406885be135e64890a8d9572da834
AROTIANA Steeve Mickaël <109659284+Luriost@users.noreply.github.com>
2022-07-20 12:12:03 +0300

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,41 @@
+"""
+Your module description
+"""
+"""print("Python has three numeric types: int, float, and complex")
+"""
+"""myValue="This is a string"
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+name = input("What is your name? ")
+print(name)
+color = input("What is your favorite color?  ")
+animal = input("What is your favorite animal?  ")
+print("{}, you like a {} {}!".format(name,color,animal))
+"""
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+myFruitList[2] = "orange"
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+print(myFruitList)
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Akua"])
+
+myMixedTypeList = [45, 290578, 1.02, True, "My dog is on the bed.", "45"]
+for item in myMixedTypeList:
+    print("{} is of the data type {}".format(item,type(item)))
---
+++ numeric-data.py
@@ -0,0 +1,23 @@
+"""
+Your module description
+"""
+"""print("Python has three numeric types: int, float, and complex")
+"""
+"""myValue="This is a string"
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+name = input("What is your name? ")
+print(name)
+color = input("What is your favorite color?  ")
+animal = input("What is your favorite animal?  ")
+print("{}, you like a {} {}!".format(name,color,animal))
+"""
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+myFruitList[2] = "orange"
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+print(myFruitList)
